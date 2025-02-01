ed28485007c4f8e92a4664fff2940a08b71f405b
JonathanBuchan-VeryGood <jonathan.buchan@verygood.gmbh>
2025-02-01 19:49:30 +0100

    Added S3bucket backend

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,12 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+    name: 's3MetaDataManagement',
+    access: (allow) => ({
+        'media-files/{entity_id}/*': [
+            allow.guest.to(['read']),
+            allow.authenticated.to(['read','write']),
+            allow.entity('identity').to(['read', 'write', 'delete'])
+        ]
+    })
+});
