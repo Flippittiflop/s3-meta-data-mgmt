ae473e6c84491f656171b8761736056c9a67d16e
JonathanBuchan-VeryGood <jonathan.buchan@verygood.gmbh>
2025-02-01 18:19:16 +0100

    Added new models

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -8,11 +8,29 @@ specifies that any user authenticated via an API key can "create", "read",
 =========================================================================*/
 const schema = a.schema({
   Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+      .model({
+        content: a.string(),
+      }).authorization((allow) => [allow.publicApiKey()]),
+
+  Template: a.model({
+    name: a.string(),
+    fields: a.string(), // JSON string of field definitions
+    categoryId: a.hasMany('Category', 'templateId'),
+  }).authorization((allow) => [allow.publicApiKey()]),
+
+  Category: a.model({
+    name: a.string(),
+    templateId: a.hasOne('Template', 'categoryId'),
+  }).authorization((allow) => [allow.publicApiKey()]),
+
+  Image: a.model({
+    s3Key: a.string(),
+    s3Url: a.string(),
+    categoryId: a.hasOne('Category', 'imageId'),
+    metadata: a.string(), // JSON string of metadata values
+  }).authorization((allow) => [allow.publicApiKey()]),
 });
+
 
 export type Schema = ClientSchema<typeof schema>;
 
@@ -32,7 +50,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
